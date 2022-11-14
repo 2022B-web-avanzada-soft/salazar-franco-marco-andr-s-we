729465b293ef5dc2bd2cccb880542ea0f93b1a8c
Marco Salazar <marco.salazar02@epn.edu.ec>
2022-11-14 18:06:03 -0500

    Variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,30 @@
+//mutables e inmutables
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 12;
+numeroDos = 8;
+numeroUno = true;
+numeroDos = false;
+
+//Inmutables
+const configuracionArchivos = 'PDF';
+//configuracionArchivos = 'XML';
+
+//vamos a preferir CONST > LET > NUNCA VAR!
+
+//Tipos de variables (primitivas)
+const numero = 1; // number
+const sueldo = 1.2; // number
+const texto = 'Marco'; // "Marco" string
+const apellido = 'Salazar'; // string
+const booleano = true; //boolean
+const hijos = null; // object
+const zapatos = undefined; // undefined
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof apellido);
+console.log(typeof booleano);
+console.log(typeof hijos);
+console.log(typeof zapatos);
